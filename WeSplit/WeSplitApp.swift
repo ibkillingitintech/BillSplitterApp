04f53d0b68e2a09c6afbc089c9c09d4d09e007c0
Ibraheem <ibraheem.r@outlook.com>
2024-09-13 18:34:58 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ibraheem k. Rawlinson on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
